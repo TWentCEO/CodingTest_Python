c4d6aad6a72a260664d74bccc61b88e5ce7622cb
TWentCEO <twent1003@gmail.com>
2023-04-15 15:02:30 +0900

    BrowserHistory update
    update

--- BrowerHistory.py
+++ BrowerHistory.py
@@ -3,22 +3,24 @@ class ListNode(object):
         self.next = next
         self.val = val
         self.prev = prev
+
+
 class BrowserHistory(object):
     def __init__(self, homepage):
-        self.head = self.current = ListNode(val = homepage)
+        self.head = self.current = ListNode(val=homepage)
+
     def visited(self, url):
-        self.current.next = ListNode(val = url, prev=self.current)
+        self.current.next = ListNode(val=url, prev=self.current)
         self.current = self.current.next
+
     def forward(self, steps):
         while steps > 0 and self.current.next is not None:
             steps -= 1
             self.current = self.current.next
         return self.current.val
+
     def back(self, steps):
         while steps < 0 and self.current.prev is not None:
             steps -= 1
             self.current = self.current.prev
         return self.current.val
-
-
-
